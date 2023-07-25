1f360f254e59de1c842da0e2726e9183ff6a7997
J-PBoucher <126096326+J-PBoucher@users.noreply.github.com>
2023-07-25 14:35:28 -0400

    BureauJP

--- ad_hoc.R
+++ ad_hoc.R
@@ -1,14 +1,18 @@
 dbfictif <- tar_read(dbfictif)
 
-data1 <- dbfictif[1:4000,]
-data2 <- dbfictif[4001:8000,]
+data1 <- dbfictif[1:40000,]
+data2 <- dbfictif[40001:80000,]
 
 ###
 
 df1 <- 
   data1 %>%
-  select(expo, freq_paiement, an_naissance, annee_veh, annee_permis, sexe, voiture) 
+  select(expo, freq_paiement:cheveux) 
+glimpse(df1)
 
+install.packages('skimr')
+skimr::skim(df1)
+  
 rec_class <-
   recipe(~ ., data = df1) %>%
   step_impute_mode(sexe) %>% 
@@ -18,6 +22,9 @@ rec_class <-
 
 x_cols <- juice(rec_class)
 nombrecol <- ncol(x_cols)
+
+colnames(dbfictif)
+
 ###
 
 db_fictif_ds <- Dataset(data1)
@@ -27,7 +34,7 @@ mlp$forward(db_fictif_ds$x[1:2, ])
 
 Mod <- PoissonMLP$new(spec=PoissonMLP_1L, dataset=Dataset)
 
-Mod$train(train=data1, valid=data2, epochs=20, lr_start=0.01, factor=0.5, patience=2, batch = 64, input_size = nombrecol, n_1L = 4)
+Mod$train(train=data1, valid=data2, epochs=5, lr_start=0.01, factor=0.5, patience=2, batch = 64, input_size = nombrecol, n_1L = 4)
 
 Mod$plot_training()
 Mod$print_metrics()
